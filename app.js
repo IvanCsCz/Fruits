f06dfecbdb544c33b9d425c4b0f39747d52fa501
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-02-05 15:54:15 -0600

    li element created everytime a fruit is typed

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+const formFruit = document.querySelector('#enteredValue form');
+const fruitList = document.querySelector('#fruitsSection ul');
+
 const getFruit = (e) => {
   e.preventDefault();
-  console.log(e.target.fruitValue.value);
+  newFruit(e.target.fruitValue.value);
   e.target.fruitValue.value = '';
 };
 
-const formFruit = document.querySelector('.entered__value form');
+const deleteFruit = (e) => {
+  e.target.remove();
+};
+
+const newFruit = (fruit) => {
+  const li = document.createElement('li');
+  li.textContent = fruit;
+  li.addEventListener('click', deleteFruit);
+  fruitList.appendChild(li);
+};
 
 formFruit.addEventListener('submit', getFruit);
